a7b3099ad51abe0a3b8f967fb3d5a4a6c2add4c3
pekoto <graham20@gmail.com>
2018-02-15 22:02:33 +0900

    Loading form basics

---
+++ FastFuzzyStringMatcher/ExampleApp/Model/LoadingStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApp.Model
+{
+    public class LoadingStatus
+    {
+        public int PercentComplete { get; set; }
+        public int TranslationsLoaded { get; set; }
+        public int TotalTranslationsToLoad { get; set; }
+    }
+}
---
+++ FastFuzzyStringMatcher/ExampleApp/View/LoaderForm.cs
@@ -0,0 +1,50 @@
+﻿using ExampleApp.Model;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExampleApp.View
+{
+    public partial class LoaderForm : Form
+    {
+        private IProgress<LoadingStatus> _progressHandler;
+        private CancellationTokenSource _cancellationToken;
+
+        public LoaderForm()
+        {
+            InitializeComponent();
+
+            _progressHandler = new Progress<LoadingStatus>(ReportProgress);
+            _cancellationToken = new CancellationTokenSource();
+        }
+
+        private async void LoaderForm_Load(object sender, EventArgs e)
+        {
+            await LoadTranslationsAsync(_cancellationToken.Token);
+
+            // TODO Show main form
+        }
+
+        private void ReportProgress(LoadingStatus loadingStatus)
+        {
+            loading_progressbar.Value = loadingStatus.PercentComplete;
+            status_lbl.Text = $"Loaded {loadingStatus.TranslationsLoaded}/{loadingStatus.TotalTranslationsToLoad}...";
+        }
+
+        private async Task LoadTranslationsAsync(CancellationToken cancellation)
+        {
+            // TODO load translations line by line
+            // Report progress
+            // Offer cancelation
+            // Pass controller to form
+
+            cancellation.ThrowIfCancellationRequested();
+        }
+
+        private void cancel_btn_Click(object sender, EventArgs e)
+        {
+            _cancellationToken.Cancel();
+        }
+    }
+}
